329f2530bcc873598ca2d94c4d0489f60fdeac8a
SevRostomyan <41015978+SevRostomyan@users.noreply.github.com>
2023-08-18 01:11:52 +0200

    Stylade Button komponenten ytterligare

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import {Pressable, Text, View} from 'react-native';
+import {Pressable, Text, View, StyleSheet } from 'react-native';
 
 const Button = ({title, onPress}) => {
   return (
-    <Pressable onPress={onPress}>
-      <View style={{padding: 10, backgroundColor: 'blue'}}>
-        <Text style={{color: 'white'}}>{title}</Text>
+      <Pressable onPress={onPress} style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }, styles.button]}>
+          <View style={styles.buttonInner}>
+              <Text style={styles.buttonText}>{title}</Text>
       </View>
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    buttonInner: {
+        backgroundColor: '#636e72',  // A soft gray color
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '500',
+    },
+});
 
 export default Button;
